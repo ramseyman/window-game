b3a3564e6a654f99a13a44e258c91d7d5961b619
elisal <98366567+HtebazilePanda@users.noreply.github.com>
2022-11-14 15:21:38 -0500

    Created a level selector, pause menu with esc

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+
+    void Start(){}
+    
+    public void OpenScene(){
+        SceneManager.LoadScene("Level " + level.ToString());
+
+    }
+}
--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -5,20 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] GameObject pauseMenu;
+    public static bool GameIsPaused = false;
 
-    public void Pause(){
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            } else{
+                Pause();
+            }
+        
+        }
     }
 
     public void Resume(){
-        pauseMenu.SetActive(false);
+        pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 
-    public void Home(int sceneID){
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu(int sceneID){
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
     }
 }
