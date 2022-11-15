7e96e326ac4c316fe715f0f9549b814578d097d9
ramseyman <94194585+ramseyman@users.noreply.github.com>
2022-11-14 22:56:43 -0500

    Add files via upload

---
+++ Assets/ViewSwitchManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSwitchManager : MonoBehaviour
+{
+    public bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Viewed(){
+        active = true;
+    }
+
+    public void Unviewed(){
+        active = false;
+    }
+}
